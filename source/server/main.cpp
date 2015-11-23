ea02cae3b6db8860d4ed07d335bc6fb9de4f4dbf
bury_a <anthony.bury@epitech.eu>
2015-11-23 13:07:18 +0100

    TEST : main for testing

--- source/server/main.cpp
+++ source/server/main.cpp
@@ -5,10 +5,29 @@
 // Login   <anthony.bury@epitech.eu>
 // 
 // Started on  Mon Nov 16 02:08:18 2015 Anthony Bury
-// Last update Mon Nov 16 02:08:26 2015 Anthony Bury
+// Last update Mon Nov 23 12:53:15 2015 Anthony Bury
 //
 
-int main(void)
-{
+#include <iostream>
+#include "StartUp.hpp"
 
+int main(int ac, char **av)
+{
+  StartUp *server;
+  if (ac == 1)
+    {
+      std::cerr << "Missing argument" << std::endl;
+      std::cerr << "Usage : " << av[0] << " PORT" << std::endl;
+      return (1);
+    }
+  try
+    {
+      server = new StartUp(av[0]);
+    }
+  catch (const std::exception &error)
+    {
+      return (1);
+    }
+  server->loop();
+  return (0);
 }

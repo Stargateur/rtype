c48919b600989ea6b5918cf570fe6aa989388a42
tacite_d <tacite_d@Akagouan.(none)>
2015-12-07 15:46:42 +0100

    tptp

---
+++ source/client/IView.cpp
@@ -0,0 +1,16 @@
+//
+// IView.cpp for IView in /home/bury_a/rendu/rtype
+// 
+// Made by Anthony Bury
+// Login   <anthony.bury@epitech.eu>
+// 
+// Started on  Tue Nov 17 12:24:41 2015 Anthony Bury
+// Last update Tue Nov 17 12:24:55 2015 Anthony Bury
+//
+
+#include "IView.hpp"
+
+IView::~IView(void)
+{
+
+}

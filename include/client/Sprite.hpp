b6e4f3b85657c5443f2d5b6a1afc017c11efaca6
costa_b <costa_b@epitech.eu>
2015-12-23 13:34:33 +0100

    add sprite

--- include/client/Sprite.hpp
+++ include/client/Sprite.hpp
@@ -6,8 +6,10 @@
 class Sprite : public AElement, public sf::Sprite
 {
   sf::Texture text;
+  int sprite_x = 68;
+  int sprite_y = 0;
 public:
-  Sprite(int pos_x, int pos_y, const std::string &texture, const std::string &name);
+  Sprite(int pos_x, int pos_y, const std::string &texture, const std::string &name, std::vector<AElement *> tmp);
   ~Sprite();
 
 public:
--- source/client/main.cpp
+++ source/client/main.cpp
@@ -5,7 +5,7 @@
 // Login   <anthony.bury@epitech.eu>
 // 
 // Started on  Mon Nov 16 02:08:18 2015 Anthony Bury
-// Last update Wed Dec  9 16:22:05 2015 Antoine Plaskowski
+// Last update Wed Dec 23 13:33:23 2015 Kevin Costa
 //
 
 #if				defined _WIN32 || defined _WIN64
@@ -28,7 +28,6 @@ int	main(void)
 #endif
 {
 	View billy;
-	std::cout << "bonjour billy" << std::endl;
  	if (!billy.init())
 		return (1);
 	billy.loop();
